bfcc5e90540c9f183be77ffeb50ad1d5c160dd09
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-09-13 23:08:18 -0300

    Testando o star rating

--- src/components/StarRating/__tests__/StarRating.test.tsx
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -3,7 +3,25 @@ import {render} from '@testing-library/react-native';
 import {StarRating} from '../StarRating';
 
 describe('StarRating', () => {
-  test('the component rendered', () => {
-    render(<StarRating rating={{average: 7}} />);
+  describe('rating was passed', () => {
+    test('show the average', () => {
+      const {getByText} = render(<StarRating rating={{average: 7}} />);
+      expect(getByText('7')).toBeTruthy(); // Esse valor existe? está na tela?
+    });
+
+    test('show the star icon', () => {
+      const {getByTestId} = render(<StarRating rating={{average: 7}} />);
+
+      expect(getByTestId('starIcon')).toBeTruthy();
+    });
+  });
+
+  describe('rating was NOT passed', () => {
+    it('return nothing', () => {
+      const {container} = render(<StarRating />); // component pai desse component
+      expect(container.children.length).toEqual(0); // sem "filhos"/childrens, vazio
+    });
   });
 });
+
+// coverage => Relatorio que mostra o quanto nosso teste está cobrindo o codigo
